9eb92a35302dab683b8d181780c7cd8a934bae4a
Simeon <mrayendi1@gmail.com>
2024-01-12 04:16:02 -0800

    initial commit

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -1,36 +1,58 @@
 import { useState } from "react";
-import React from "react";
 import axios from 'axios';
 
 const Data = () => {
-  const [resumeData, setResumeData] = useState ([]);
-  const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    user: '',
+    email: '',
+    full_name: ''
+  });
 
-  const handleSubmit = async (event) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setIsLoading(false);
-
+    console.log("Form submitted");
+    alert('Form Submitted');
+    console.log(formData);
     try {
-      
-      const response = await axios.post('http://127.0.0.1:8000/core/personal-information/', resumeData, {
-        headers: {
-          //Authorization: `Token ${token}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      setResumeData(response.data);
-    } catch (err){
-      setError
-
+      const response = await axios.post("http://127.0.0.1:8000/core/personal-information/", formData);
+      console.log(response.data);
+      alert('Api Request sent');
+    } catch (error) {
+      console.error('There was an error!', error);
     }
-  }
+  };
+
   return (
-    
-    <>
-      
-    </>
-  )
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="user"
+        value={formData.user}
+        onChange={handleInputChange}
+      />
+      <input
+        type="text"
+        name="full_name"
+        value={formData.full_name}
+        onChange={handleInputChange}
+      />
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default Data;
