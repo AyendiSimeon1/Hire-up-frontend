b86667f939d7a12c60adfcf0e4ffac7aefcb2aa9
Simeon <mrayendi1@gmail.com>
2024-01-19 03:51:19 -0800

    update to project form

--- src/components/ProjectForm.tsx
+++ src/components/ProjectForm.tsx
@@ -9,13 +9,13 @@ interface ProjectFormsProps {
 
 interface ProjectForm {
   
-  full_name?: string;
-  email?: string;
   user?: string;
-  phone_number?: string;
-  address?: string;
-  linkedin_profile?: string;
-  github_profile?: string;
+  project_name?: string;
+  description?: string;
+  start_date?: string;
+  end_date?: string;
+  url?: string;
+ 
   
 }
 
@@ -34,32 +34,62 @@ const ProjectForms: React.FC<ProjectFormsProps> = ({ handleChange, ProjectForm }
       <form>
       
         <div>
-          <label htmlFor="Full Name">Full Name:</label>
-          <input
-            type="text"
-            id="full_name"
-            name="full_name"
-            value={ProjectForm.full_name || ''}
-            onChange={onInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={ProjectForm.email || ''}
-            onChange={onInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="user">User:</label>
+          <label htmlFor="user">user</label>
           <input
             type="text"
             id="user"
             name="user"
             value={ProjectForm.user || ''}
+            onChange={onInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="project_name">project_name:</label>
+          <input
+            type="text"
+            id="project_name"
+            name="project_name"
+            value={ProjectForm.project_name || ''}
+            onChange={onInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="description">description:</label>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            value={ProjectForm.description || ''}
+            onChange={onInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="start_date">start_date:</label>
+          <input
+            type="text"
+            id="start_date"
+            name="start_date"
+            value={ProjectForm.start_date || ''}
+            onChange={onInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="end_date">end_date:</label>
+          <input
+            type="text"
+            id="end_date"
+            name="end_date"
+            value={ProjectForm.end_date || ''}
+            onChange={onInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="url">url:</label>
+          <input
+            type="text"
+            id="url"
+            name="url"
+            value={ProjectForm.url || ''}
             onChange={onInputChange}
           />
         </div>
