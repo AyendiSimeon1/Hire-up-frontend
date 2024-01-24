48e6f375b3cdb89b527a5708af7b091332659bb8
Simeon <mrayendi1@gmail.com>
2024-01-24 06:48:37 -0800

    created components for homepage layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 //import { useState } from 'react'
 import ResumeForm from './components/data.tsx';
+import Homepage from './components/home.tsx';
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   
 
   return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/resume" element={<ResumeForm />} />
+          
+        </Routes>
+      </div>
+    </Router>
     
-   
-      <ResumeForm />
+  
    
   )
 }
