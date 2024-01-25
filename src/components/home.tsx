216f40dd515cff4a7ebaee83d17f87e1c86d1b0d
Simeon <mrayendi1@gmail.com>
2024-01-25 12:54:50 -0800

    initial commit

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,15 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './layouts/navbar/navbar.tsx';
-
+import Hero from './layouts/hero/hero.tsx';
 
 const Homepage:React.FC = () => {
     return(
+        <>
         <Navbar />
+
+        <Hero />
+        </>
     )
 
 
 }
 
-// #34568B #FFFFFF #343A40
+//  #34568B #FFFFFF #343A40
 
 export default Homepage;
